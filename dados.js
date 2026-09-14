f0ac7debe97a9798ad631132497ced5b0e9737b4
luisleiteflash <luis.leite@move3.com.br>
2026-09-14 09:56:50 -0300

    Atualiza dados.js - 14/09/2026 09:56

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
 /* gerado automaticamente a partir da planilha - nao editar a mao */
 window.FAT = {
-  atualizado: "10/09/2026 18:56",
+  atualizado: "14/09/2026 09:56",
   arquivo: "HISTORICO_FATURAMENTO.XLS.xlsx",
   meses: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
   anos: [
